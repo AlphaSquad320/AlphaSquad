8f49bdb24ca4cce6778902fa0bf06567d74d454a
Tommy Bohde <txb2164@rit.edu>
2016-04-13 20:47:45 -0400

    Added menu bar framework. Most functionality not yet implemented.

--- src/as/project/gui/UserWindow.java
+++ src/as/project/gui/UserWindow.java
@@ -16,17 +16,31 @@ import java.awt.event.ActionListener;
  */
 public class UserWindow extends JFrame implements ActionListener
 {
+	private JMenuBar menuBar = new JMenuBar();
+	private JMenu menuFile = new JMenu("File");
+	private JMenu menuUser = new JMenu("User");
+	private JMenuItem fileExit = new JMenuItem("Exit");
+	private JMenuItem userLogIn = new JMenuItem("Log in");
+	private JMenuItem userLogOut = new JMenuItem("Log out");
+
+	private boolean userLoggedIn = false;
+	
 	public UserWindow()
 	{
 		//build the frame
 		super("Welcome");
 
 		//layout stuff
+		layOutWindow();
+
+		//set up action listeners
+		fileExit.addActionListener(this);
+		userLogIn.addActionListener(this);
+		userLogOut.addActionListener(this);
 
 		//final housekeeping
 		setSize(new Dimension(800, 600));
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		//pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
 
@@ -36,13 +50,26 @@ public class UserWindow extends JFrame implements ActionListener
 
 	}
 
+	private void layOutWindow()
+	{
+		//set up the menu bar
+		this.setJMenuBar(menuBar);
+		menuBar.add(menuFile);
+		menuBar.add(menuUser);
+		menuFile.add(fileExit);
+		menuUser.add(userLogIn);
+		menuUser.add(userLogOut);
+	}
+
 	public boolean authenticateUser(String username, String password)
 	{
 		System.out.println("Authenticating user " + username +
 							" with password "  + password);
-		if (true) //TODO: SQL to find user in database
+		//TODO: SQL to find user in database
+		if (true) 
 		{
 			loadUser(username);
+			userLoggedIn = true;
 			return true;
 		}
 		//if not found
@@ -59,6 +86,36 @@ public class UserWindow extends JFrame implements ActionListener
 
 	public void actionPerformed(ActionEvent e)
 	{
-
+		if (e.getSource() == fileExit)
+		{
+			//TODO: shutdown function
+			//  - log out user
+			//  - save/close database
+			//  - clean program exit
+		}
+		else if (e.getSource() == userLogIn)
+		{
+			if (userLoggedIn)
+			{
+				//TODO
+				//log out user? prompt? nothing?
+			}
+			else
+			{
+				//TODO: display login window
+			}
+		}
+		else if (e.getSource() == userLogOut)
+		{
+			if (userLoggedIn)
+			{
+				//TODO: log out user
+			}
+			else
+			{
+				//TODO
+				//prompt? nothing?
+			}
+		}
 	}
 }
