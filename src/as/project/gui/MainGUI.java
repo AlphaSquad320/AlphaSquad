68421f5aac0ecde79e53694c1f3283bcdd053120
Tommy Bohde <txb2164@rit.edu>
2016-04-26 18:19:52 -0400

    Added execution of project.Main.main() to create the sample database.

--- src/as/project/gui/MainGUI.java
+++ src/as/project/gui/MainGUI.java
@@ -1,4 +1,4 @@
-/* File: UserWindow.java
+/* File: MainGUI.java
  * 
  * TODO: Merge this file into another (possible UserWindow?)
  */ 
@@ -8,6 +8,8 @@ package as.project.gui;
 import javax.swing.*;
 import java.awt.*;
 
+import as.project.Main;
+
 /**
  * This class is the main class of the GUI package. Instantiates a UserWindow
  * and makes it visible on screen.
@@ -16,7 +18,12 @@ public class MainGUI
 {
 	public static void main(String[] args)
 	{
+		//Create and display main user window
 		UserWindow login = new UserWindow();
 		login.setVisible(true);
+
+		//execute as.project.Main.main() to create the sample database
+		Main main = new Main();
+		main.main(new String[0]);
 	}
 }
