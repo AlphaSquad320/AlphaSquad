0afb46b7a44352a62347735542fc776d0fedeb25
Lukas Hillmer <lukas.hillmer@gmail.com>
2016-05-02 17:18:20 -0400

    adding in the use of variables for easy column access

--- src/as/project/tables/ItemTable.java
+++ src/as/project/tables/ItemTable.java
@@ -12,9 +12,19 @@ import java.util.List ;
 
 //project imports
 import as.project.objects.Item ;
+import as.project.objects.User;
 
 public class ItemTable extends TableBase
 {
+	public static final String TABLE_NAME = "item";
+	public static final String ID_COLUMN = "ITEM_ID";
+	public static final String EFFECTS_COLUMN = "ADD_EFF";
+	public static final String DESCRIPTION_COLUMN = "DESC";
+	public static final String CONSUMABLE_COLUMN = "CONS";
+	public static final String TYPE_COLUMN = "TYPE";
+	public static final String BONUS_COLUMN = "BONUS";
+	public static final String ITEM_CLASS_COLUMN = "ITEM_CLASS";
+	
   /**
    * createItemTable method
    * create item table with given attributes
@@ -23,14 +33,14 @@ public class ItemTable extends TableBase
   **/
   public static void createItemTable( Connection conn ) throws SQLException
   {
-    String query = "CREATE TABLE IF NOT EXISTS item("
-      + "ITEM_ID INT PRIMARY KEY,"
-      + "ADD_EFF VARCHAR(255),"
-      + "DESC VARCHAR(255),"
-      + "CONS BOOLEAN,"
-      + "TYPE VARCHAR(255),"
-      + "BONUS INT,"
-      + "ITEM_CLASS VARCHAR(255),"
+    String query = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "("
+      + ID_COLUMN + " INT PRIMARY KEY,"
+      + EFFECTS_COLUMN + " VARCHAR(255),"
+      + DESCRIPTION_COLUMN + " VARCHAR(255),"
+      + CONSUMABLE_COLUMN + " BOOLEAN,"
+      + TYPE_COLUMN + " VARCHAR(255),"
+      + BONUS_COLUMN + " INT,"
+      + ITEM_CLASS_COLUMN + " VARCHAR(255),"
       + ");" ;
 
     Statement state = conn.createStatement() ;
