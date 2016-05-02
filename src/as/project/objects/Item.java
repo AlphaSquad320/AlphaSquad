cc71b79fcec718678cebaabfba5ecd8983029b68
Lukas Hillmer <lukas.hillmer@gmail.com>
2016-05-02 17:20:53 -0400

    removing reqLevel field from Item obj, as no field was created for it in the db table

--- src/as/project/objects/Item.java
+++ src/as/project/objects/Item.java
@@ -5,7 +5,6 @@ public class Item
   private int itemID ;
   private String addEff ; //Additional Effects
   private String desc ;   //Description
-  private int reqLevel ;  //Required Level
   private boolean cons ;  //Consumable
   private String type ;
   private int bonus ;
@@ -17,7 +16,6 @@ public class Item
    * @param ItemID - Item ID number
    * @param AddEff - Additional Effect text string
    * @param Desc - Item description
-   * @param ReqLevel - Required level for item use
    * @param Cons - Is item consumable
    * @param Type - Item type (Armor, Weapon, etc.)
    * @param Bonus - Item stat bonus amount
@@ -27,7 +25,6 @@ public class Item
     int ItemID, 
     String AddEff,
     String Desc,
-    int ReqLevel,
     boolean Cons,
     String Type,
     int Bonus,
@@ -36,7 +33,6 @@ public class Item
     itemID = ItemID ;
     addEff = AddEff ;
     desc = Desc ;
-    reqLevel = ReqLevel ;
     cons = Cons ;
     type = Type ;
     bonus = Bonus ;
@@ -52,9 +48,6 @@ public class Item
   public String getDescription(){ return desc ; }
   public void setDescription( String newDescription )
   { desc = newDescription ; }
-
-  public int getReqLevel(){ return reqLevel ; }
-  public void setReqLevel( int newReqLevel ){ reqLevel = newReqLevel ; }
 
   public boolean isConsumable(){ return cons ; }
   public void setConsumable( boolean newConsumable )
